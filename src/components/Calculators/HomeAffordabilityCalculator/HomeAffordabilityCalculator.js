854b8ff0b2b9c0ac0bfef0d142501650bb4bd332
Khossain01 <81064060+Khossain01@users.noreply.github.com>
2023-12-10 17:19:00 -0500

    added total home cost functionality

--- src/components/Calculators/HomeAffordabilityCalculator/HomeAffordabilityCalculator.js
+++ src/components/Calculators/HomeAffordabilityCalculator/HomeAffordabilityCalculator.js
@@ -14,6 +14,7 @@ import { useReactToPrint } from "react-to-print";
 
 import CalculatorUsageGuide from "./CalculatorUsageGuide";
 
+
 //import {PieChart } from '@mui/x-charts/PieChart';
 //import { useDrawingArea } from '@mui/x-charts/hooks';
 import { Chart as ChartJS, ArcElement,Tooltip, Legend,Title,PointElement,LineElement} from 'chart.js'
@@ -48,6 +49,7 @@ const StyledButton = styled(Button)`
     transform: translateY(-2px); 
   }
 `;
+
 const ResultDisplay = styled.p`
   margin-top: 20px;
   font-size: 24px;
@@ -75,7 +77,8 @@ const HomeAffordabilityCalculator = () => {
   const[monthlyIncome, setMonthlyIncome] = useState(0); //Initially the start value will be 0
   const[monthlyDebt, setMonthlyDebt] = useState(0);
   const[downpaymentAmt, setDownpaymentAmt] = useState(0);
-  //const[totalHomeCost, setTotalHomeCost]  = useState(0);
+  const[YearlyHouseholdIncome, setYearlyHouseholdIncome]= useState(0); //this records the user's total household income
+  const[totalHomeCost, setTotalHomeCost]  = useState(0);
   const[monthlyHomecost, setMonthlyHomeCost] = useState(0);
 
   const [ChartData, setChartData] = useState(() => ({
@@ -88,14 +91,22 @@ const HomeAffordabilityCalculator = () => {
   const openGuide = () => setModalOpen(true);
   const closeGuide = () => setModalOpen(false);
 
+
+  
+
+  const formatNumber = (value) => {
+    const options = { maximumFractionDigits: 2 };
+    return new Intl.NumberFormat("en-US", options).format(value);
+  };
+
   useEffect(() => {
     setChartData({
       labels: ['Debt', 'Home Payment', 'Other'],
       datasets: [{
         label: "Monthly Income Breakdown",
         data: [monthlyDebt, monthlyIncome * 0.28, monthlyIncome - (monthlyDebt + monthlyIncome * 0.28)],
-        backgroundColor: [blueGrey[500], lightGreen[500], '#ffbb33'],
-        borderColor: [blueGrey[700], lightGreen[700], '#ff8800'],
+        backgroundColor: ['#00ced1', '#9370db', '#8b008b'],
+        borderColor: ['#00ced1', '#9370db', '#8b008b'],
         borderWidth: 1,
       }]
     });
@@ -107,103 +118,31 @@ const HomeAffordabilityCalculator = () => {
     setMonthlyDebt(0);
     setDownpaymentAmt(0);
     setMonthlyHomeCost(0);
+    setTotalHomeCost(0);
+    setYearlyHouseholdIncome(0);
   }
 
-
-  // const data ={
-  //   labels:['Debt', 'Home Payment', 'Other'],
-  //    datasets: [{
-  //      label:"Monthly Income Breakdown",
-  //      data: [monthlyDebt, monthlyHomecost, remainderSalary],
-  //      //data: [1500,1260,3240],
-  //     //data: [3,6],
-  //      backgroundColor: ['blueGrey','lightGreen','purple'],
-  //      borderColor:['blueGrey','lightGreen','purple']
-  //    }]
-
-
-
-  //    datasets: [{
-  //       label:"Monthly Income Breakdown",
-  //       //data: [monthlyDebt, monthlyHomecost, monthlyIncome],
-  //       backgroundColor: ['darkcyan','lightGreen','purple'],
-  //       borderColor:['darkcyan','lightGreen','purple']
-  //     }]
-
   
 
- 
-
-
-
-  // const validateForm =() =>{
-  //   if(monthlyIncome.length  == 0) {
-  //     alert('Invalid Form, Monthly Income can not be empty')
-  //     return
-  //   }
- 
-  // }
- 
- 
-
-  // let calculatemonthlyhomecost =() => {
-  //   if (monthlyIncome > 0 |monthlyDebt > 0 |downpaymentAmt >= 0){
-  //        const monthlyHomeCost = (monthlyIncome - monthlyDebt) * 0.28 
-  //        setMonthlyHomeCost(parseFloat(monthlyHomecost.fixed(2))); 
-  //   }
-  //   else{
-  //     alert("Please enter valid data within the fields above ")
-  //   }
-  // };
-
-
-  // let calcmonthlyhomecost =(event) =>{
-  //   //cancel default action 
-  //   event.preventDefault();
-  
-  //   //Validations to prevent invalid input
-  //   if(monthlyIncome ===0 | monthlyDebt === 0 ){
-  //     alert('Please enter a valid input')
-  //   } else{
-  //      //Calculations to display monthly cost
-  //     let monthlyHomecost = (monthlyIncome - monthlyDebt) * 0.28   // No more than 28% of net income should go for home costs
-  //     setMonthlyHomeCost(monthlyHomecost.toFixed(2)) // returns monthly home costs in a decimal format         
-
-  //   }
-  //   //Calculations to display total home cost user can afford
-  // }
-
-  const calcmonthlyhomecost = (event) => {
+  const calcmonthlyandtotalhomecost = (event) => {
     event.preventDefault(); // Prevent default form submission behavior
-    if (monthlyIncome > 0 && monthlyDebt >= 0) {
+
+    if (monthlyIncome >= 0 && monthlyDebt >= 0) {
       const calculatedMonthlyHomeCost = (monthlyIncome - monthlyDebt) * 0.28;
       setMonthlyHomeCost(calculatedMonthlyHomeCost.toFixed(2));
+
+      const calculatedTotalHomeCost = YearlyHouseholdIncome * 3;
+      setTotalHomeCost(calculatedTotalHomeCost.toFixed(2));
+
     } else {
-      alert('Please enter a valid input for income and debt');
+      alert('Please enter a valid input for income and/or debt');
     }
+
+
+
+
   }
 
-
-
-//   const calcmonthlyhomecostS =() =>{
-//          const remainderSalary = monthlyIncome - monthlyDebt - monthlyHomecost   // "Other income"
-//       setChartData({
-//         labels: ["Debt", "Home Payment", "Other"],
-//         datasets: [
-//           {
-//             data: [monthlyDebt, monthlyHomecost, remainderSalary],
-//             backgroundColor: ['darkcyan','lightGreen','purple'],
-//             borderColor:['darkcyan','lightGreen','purple']
-//           },
-//         ],
-//       })
-// ;
-//   }
-
-
-  // let reload =() => {
-  //   window.location.reload()
-  // }
 
   const printRef = useRef();
   const handlePrint = useReactToPrint({
@@ -230,11 +169,10 @@ const HomeAffordabilityCalculator = () => {
         NumericFormat allowNegative={false} 
         margin="normal"
         onChange={(e) => {
-          const value = parseFloat(e.target.value)
-          if (!isNaN (value) && value >= 0){
-            setMonthlyIncome(value);
-          }
+          e.target.value = e.target.value < 0 ? 0 : e.target.value;
+          setMonthlyIncome(parseFloat(e.target.value));
         }}
+
       />
     <StyledTextField
         variant ="outlined"
@@ -273,13 +211,35 @@ const HomeAffordabilityCalculator = () => {
         
       />
 
+      <StyledTextField
+        variant ="outlined"
+        label="Enter Total Yearly Household Income"
+        value = {YearlyHouseholdIncome}
+        placeholder="$60,000"
+        type="number"
+        fullWidth
+        InputProps={{
+          startAdornment: <InputAdornment position="start">$</InputAdornment>,
+        }}
+        NumericFormat allowNegative={false} 
+        margin="normal"
+        onChange={(e) => {
+          e.target.value = e.target.value < 0 ? 0 : e.target.value;
+          setYearlyHouseholdIncome(parseFloat(e.target.value));
+        }}
+      /> 
+
+
+
+
+
         
     <Stack direction="row" spacing={2}> 
       <StyledButton      
         variant="contained" 
         color="success"
         endIcon={<SendIcon />}
-        onClick={calcmonthlyhomecost}
+        onClick={calcmonthlyandtotalhomecost}
       >
         Calculate
       </StyledButton>
@@ -313,62 +273,17 @@ const HomeAffordabilityCalculator = () => {
       </Stack>
       
       <ResultDisplay>You can Afford a Home with a Maximum Monthly Cost of: ${monthlyHomecost}</ResultDisplay>
+      <ResultDisplay>Affordable Home Cost: ${formatNumber(totalHomeCost)}</ResultDisplay>   
+      <div></div>
+
       <h2> Your Monthly Income Breakdown</h2>
       <div style ={{width:'40%', height: '40%', margin: 'auto', padding: '20px'}}>
         <Doughnut
           data = {ChartData}
         />
       </div>
+  
 
-
-
-      <div>
-        {/* {ChartData &&(
-          // <Doughnut
-          //   data = {ChartData} 
-          //   // options ={(
-          //   //   responsive = true
-          //   // //   plugins: {}
-          //   // //     legend:(position: "top")
-          //   // //     title:(display:true, text = "Monthly Cost Breakdown")
-          //   // // },
-
-          //   // )}
-          // />
-
-
-          <div>
-            <div
-              style={{ textAlign: "center", marginTop: 30, fontSize: "1.5em" }}
-            >
-              Debt-to-Income Ratio: {ChartData.datasets[0].data[0].toFixed(2)}%
-            </div>
-            <div style={{ width: "300px", height: "300px", margin: "auto" }}>
-              {" "}
-              <Pie data={ChartData} style={{ marginTop: 30 }} />
-            </div>
-          </div>
-
-        )} */}
-        </div>
-
-
-
-
-
-        {/* {ChartData && (
-          <div>
-            <div
-              style={{ textAlign: "center", marginTop: 30, fontSize: "1.5em" }}
-            >
-                Home Affordability Calculation: ChartData.datasets[0].data[0].toFixed(2)
-            </div>
-            <div style={{ width: "300px", height: "300px", margin: "auto" }}>
-              {" "}
-              <Pie data={ChartData} style={{ marginTop: 30 }} />
-            </div>
-          </div>
-        )} */}
     </StyledContainer>
   </div>
   );
